8e08861cf6c4898d7b1fe4f4c76d814da2f9ec94
Mert Şimşek <mertsimsekk01@gmail.com>
2024-02-07 09:30:52 +0300

    update hex.js to generate random hexColor in ColorFlipper

--- ColorFlipper/hex.js
+++ ColorFlipper/hex.js
@@ -1,2 +1,24 @@
-console.log('hex.js is on action and it works.');
-console.log('to be disabled');
+//generates random hexadecimal color on each button click
+let chars = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'A', 'B', 'C', 'D', 'E', 'F' ];
+
+const btn = document.querySelector('button');
+const color = document.getElementById('color-code');
+btn.addEventListener('click', function () {
+    const content = document.querySelector('.content');
+    let randomColor =generateColor();
+    content.style.backgroundColor = randomColor;
+    color.textContent = randomColor;
+})
+
+function generateColor(){
+    let randomColor ='#';
+    for (let i = 0; i < 6; i++) {
+        randomColor += chars[getRandomIndex()];
+        console.log(randomColor);
+    }
+    return randomColor;
+}
+
+function getRandomIndex() {
+    return Math.floor(Math.random() * chars.length);
+}
